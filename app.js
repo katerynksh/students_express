7c94beb8a1552e584599721ffe30e3c6cfaa236e
classuser320 <stanislav.v.romaniuk@ukd.edu.ua>
2026-03-23 10:56:44 +0200

    add: street food app

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ import carsRouter from './routes/cars.js'
 import slonikiRouter from './routes/sloniki.js'
 import dhdRouter from './routes/dhd.js'
 import catsRouter from './routes/cats.js'
+import streetFoodRouter from './routes/street_food.js'
 
 import { fileURLToPath } from 'url';
 
@@ -39,13 +40,14 @@ app.use('/sloniki', slonikiRouter)
 app.use('/cars', carsRouter);
 app.use('/dhd', dhdRouter)
 app.use('/cats', catsRouter)
+app.use('/street_food', streetFoodRouter)
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
